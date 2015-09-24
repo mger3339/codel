5e8ac930c467732723c6c97ca6995a27b3662dfa
mger33389 <mger33389@mail.ru>
2015-09-24 21:06:22 +0400

    add search result page

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -7,14 +7,23 @@ class Search extends CI_Controller
     {
 
         if($this->input->get('submit')) {
-           $data = $this->input->get();
+            $data = $this->input->get();
             unset($data['submit']);
             $this->load->model('frontend/search_model');
-            $result = $this->search_model->search($data);
-            echo "<pre>";
-            print_r($result); die;
-            }
+            $this->load->model('frontend/products_model');
+            $result['data'] = $this->search_model->search($data);
+            $first_name = $this->session->userdata('first_name');
+            $last_name = $this->session->userdata('last_name');
+            $user_id = $this->session->userdata('user_id');
+            $home['user_data'] = array('first_name' => $first_name, 'last_name' => $last_name);
+            $home['cart'] = $this->products_model->getCartProductByUserId($user_id);
+            $result['areas'] = $this->products_model->getAreas();
+            $result['categories'] = $this->products_model->getCategories();
+            $this->load->view('frontend/header_view', $home);
+            $this->load->view('frontend/search_result_view', $result);
+            $this->load->view('frontend/footer_view');
         }
+    }
 //        }
 //        if(empty($area) && empty($category) && empty($text))
 //        {
--- application/views/frontend/search_result_view.php
+++ application/views/frontend/search_result_view.php
@@ -69,15 +69,6 @@
             <div class="asd"></div>
         </div>
     <?php endforeach; ?>
-    <script>(function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1672897846273496";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
-    <div class="fb-like" data-href="https://shop.dev/home" data-layout="standard" data-action="like" data-colorscheme="dark" data-show-faces="true" data-share="true"></div>
-    <div class="pagin"><?php echo $this->pagination->create_links(); ?></div>
 </div>
 </body>
 </html>
